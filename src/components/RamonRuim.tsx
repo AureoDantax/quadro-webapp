0d60471af04d2432d8fae13641d655fe7d5d19fa
Aureo Dantas <dantasaureo19@gmail.com>
2024-08-24 16:11:24 -0300

    preaprando o projeto

---
+++ src/components/RamonRuim.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const RamonRuim = styled.div`
+width: 100%;
+font-size: 70px;
+font-family: Arial, Helvetica, sans-serif;
+background-color: #FF0000;
+`;
+
+const HelloWorld = () => {
+return(
+    <RamonRuim>SABE NEM FAZER O BASICO MLK!!</RamonRuim>
+);
+}
+export default HelloWorld
